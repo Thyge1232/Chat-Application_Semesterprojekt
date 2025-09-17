8eff89901b677f9514cc30a4360f50906a5c80bb
Nis Studie <bruger@MacBook-Pro-tilhrende-Bruger.local>
2025-09-17 12:06:48 +0200

    added frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import { DemoComponent } from "./demo";
+
+function App() {
+  return <DemoComponent />;
+}
+
+export default App;
---
+++ frontend/src/demo.tsx
@@ -0,0 +1,12 @@
+export function DemoComponent() {
+  return (
+    <div className="flex flex-col items-center">
+      <h3 className="text-xl font-mono">223</h3>
+      <h1 className="text-sm">22</h1>
+    </div>
+  );
+}
+// TODO FIX TAILWIND
+// https://www.reddit.com/r/tailwindcss/comments/1fb1swg/is_there_a_quicker_way_to_set_up_react_projects/
+// maybe redo this entire project.
+// todo also tailwind for the choir-app??
