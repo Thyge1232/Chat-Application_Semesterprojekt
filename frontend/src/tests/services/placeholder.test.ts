28340403f3163459a78449dba5533a6813f10df2
Nis Studie <bruger@MacBookPro.lan>
2025-09-17 15:03:50 +0200

    added example testagain

--- frontend/src/tests/services/placeholder.test.ts
+++ frontend/src/tests/services/placeholder.test.ts
@@ -3,3 +3,7 @@ import { it, describe, expect } from "vitest";
 it("can check that true is true", () => {
   expect(true).toBe(true);
 });
+
+it("can check that false is false", () => {
+  expect(false).toBe(false);
+});
