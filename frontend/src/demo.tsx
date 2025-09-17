d38dac1779beaecafa67d9287395fa06d1b8f8ca
Nis Studie <bruger@MacBookPro.lan>
2025-09-17 14:06:45 +0200

    changed reame

--- frontend/src/demo.tsx
+++ frontend/src/demo.tsx
@@ -1,12 +1,8 @@
 export function DemoComponent() {
   return (
     <div className="flex flex-col items-center">
-      <h3 className="text-xl font-mono">223</h3>
-      <h1 className="text-sm">22</h1>
+      <h3 className="text-xl font-mono">testsetest</h3>
+      <h1 className="text-xl">bigger text</h1>
     </div>
   );
 }
-// TODO FIX TAILWIND
-// https://www.reddit.com/r/tailwindcss/comments/1fb1swg/is_there_a_quicker_way_to_set_up_react_projects/
-// maybe redo this entire project.
-// todo also tailwind for the choir-app??
