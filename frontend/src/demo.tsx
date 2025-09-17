73046d49373e954aaea214fe5d5dc75aa7e941ae
Nis Studie <bruger@MacBookPro.lan>
2025-09-17 14:09:10 +0200

    test

--- frontend/src/demo.tsx
+++ frontend/src/demo.tsx
@@ -2,7 +2,7 @@ export function DemoComponent() {
   return (
     <div className="flex flex-col items-center">
       <h3 className="text-xl font-mono">testsetest</h3>
-      <h1 className="text-xl">bigger text</h1>
+      <h1 className="text-xl">bigger text again</h1>
     </div>
   );
 }
