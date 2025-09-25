f573175d25e576dba7f7062a8e84ea0eb5659828
Line H <201304102@post.au.dk>
2025-09-25 10:51:36 +0200

    photo+login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,16 @@
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Demo } from "./pages/Demo";
+import { Login } from "./pages/Login";
 
 const App = () => {
-  return <Demo/>;
-};
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Demo />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+}
 
 export default App;
