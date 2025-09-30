056b62d707479cefe6a19d786cdc08a2dca60217
Thyge Bertelsen <202209967@post.au.dk>
2025-09-30 08:10:58 +0200

    1

--- BackendAPI/Services/Mocks/MockUserService.cs
+++ BackendAPI/Services/Mocks/MockUserService.cs
@@ -5,17 +5,15 @@ using BackendAPI.Services.Interfaces;
 
 namespace BackendAPI.Services.Mocks
 {
-    // Denne klasse implementerer det SAMME interface som jeres rigtige service.
-    // Den er en "stunt double".
+
     public class MockUserService : IUserService
     {
-        // Vi simulerer en database med en simpel liste i hukommelsen.
         private readonly List<UserDto> _users;
         private int _nextId = 3; // Start ID efter de hardcodede brugere
 
         public MockUserService()
         {
-            // Initialiser med noget falsk data
+            
             _users = new List<UserDto>
             {
                 new UserDto { Id = 1, Username = "mock_alice", Email = "alice@mock.com", CreatedAt = DateTime.UtcNow },
@@ -26,7 +24,6 @@ namespace BackendAPI.Services.Mocks
         public Task<UserDto?> GetUserByIdAsync(int userId)
         {
             var user = _users.FirstOrDefault(u => u.Id == userId);
-            // Task.FromResult() pakker et simpelt resultat ind i en Task, så signaturen matcher interfacet.
             return Task.FromResult(user);
         }
 
@@ -37,7 +34,6 @@ namespace BackendAPI.Services.Mocks
 
         public Task<UserDto> RegisterUserAsync(CreateUserDto createUserDto)
         {
-            // Simuler at tjekke for dubletter
             if (_users.Any(u => u.Username == createUserDto.Username))
             {
                 throw new ArgumentException($"Username '{createUserDto.Username}' is already taken.");
