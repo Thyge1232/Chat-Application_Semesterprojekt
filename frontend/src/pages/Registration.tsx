434e9d8476d0fbd6a317be291ebb247d481dfdd4
Line H <201304102@post.au.dk>
2025-10-01 16:21:16 +0200

    started registration page

--- frontend/src/pages/Registration.tsx
+++ frontend/src/pages/Registration.tsx
@@ -1,3 +1,30 @@
+import type {UserRegistration} from "../types/userregistration";
+import { useForm } from "react-hook-form";
+import type { SubmitHandler } from "react-hook-form";
+import {useNavigate } from "react-router-dom"; 
+
+
+type FormValues = UserRegistration & {  confirmPassword: string };
+
+
 export const Registration = () => {
-  return <div> hey register</div>;
+ const { 
+    register, 
+    handleSubmit, 
+    formState: { errors } 
+  } = useForm<FormValues>();
+
+  const navigate = useNavigate();
+
+  const onSubmit: SubmitHandler<FormValues> = () => {
+    console.log();
+    //TODO: validate against database - does this user exist?
+    navigate("/conversations");
+  };
+ 
+  return (
+    <div> HELLO REG </div>
+  )
+
+
 };
