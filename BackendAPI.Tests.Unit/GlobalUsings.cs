e3a9b50aa87eda10488a49adc385bd46a3d62da1
Thyge Bertelsen <202209967@post.au.dk>
2025-10-04 12:41:01 +0200

    Created a new unit test project: BackendAPI.Tests.Unit.

---
+++ BackendAPI.Tests.Unit/GlobalUsings.cs
@@ -0,0 +1,10 @@
+//xUnit's kerne-funktionalitet globalt tilgængelig i alle testfiler
+global using Xunit;
+
+global using Moq;
+
+global using BackendAPI.Dtos;
+global using BackendAPI.Services.Interfaces;
+global using BackendAPI.Services.Implementations;
+global using BackendAPI.Repositories.Interfaces;
+global using BackendAPI.Models;
