e359f637ad74ae1bd808ba9b74a4ec6e02334c84
Thyge Bertelsen <202209967@post.au.dk>
2025-10-04 12:59:59 +0200

    1

--- BackendAPI/Models/User.cs
+++ BackendAPI/Models/User.cs
@@ -4,11 +4,11 @@ public class User
 {
     public int UserId { get; set; }
 
-    public string Username { get; set; } = null!;
+    public string Username { get; set; } = string.Empty;
     public string Password { get; set; } 
     public DateTime CreatedAt { get; set; }
-    public string? ProfilePicture { get; set; }
-    public string Email { get; set; }= null!;
+    public string ProfilePicture { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
     public ICollection<ConversationMember> MemberIn { get; set;} = new List<ConversationMember>();
 
 }
