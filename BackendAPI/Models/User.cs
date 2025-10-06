ec76577992a5e72a6cdc3d5f8c1a4f1faafefceb
Nikolaj <nikoh@NikolajsPC.localdomain>
2025-10-06 11:14:58 +0200

    minor changes to BackendAPI.Tests.Integration

--- BackendAPI/Models/User.cs
+++ BackendAPI/Models/User.cs
@@ -3,7 +3,6 @@ namespace BackendAPI.Models;
 public class User
 {
     public int UserId { get; set; }
-
     public string Username { get; set; } = string.Empty;
     public string Password { get; set; } 
     public DateTime CreatedAt { get; set; }
