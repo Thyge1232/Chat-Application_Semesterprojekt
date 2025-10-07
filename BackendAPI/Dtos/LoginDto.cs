ac595ad3110cbc03b8994995d252cd5367e07612
Thyge Bertelsen <202209967@post.au.dk>
2025-10-07 22:12:44 +0200

    Initialize Username property in LoginDto with an empty string

--- BackendAPI/Dtos/LoginDto.cs
+++ BackendAPI/Dtos/LoginDto.cs
@@ -4,7 +4,7 @@ namespace BackendAPI.Dtos
     public class LoginDto
     {
         [Required(ErrorMessage = "Username is required")]
-        public string Username { get; set; }
+        public string Username { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Password is required")]
         public string Password { get; set; }
