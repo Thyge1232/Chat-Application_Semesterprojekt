ecfd0fa9977efb33785dd35c2c51e06d800957dd
Daniel Machado <au280502@uni.au.dk>
2025-11-08 12:31:17 +0100

    excluded several files from vitest in the vitest.config

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -9,6 +9,13 @@ export default defineConfig({
     setupFiles: "./tests/setupTests.ts",
     coverage: {
       provider: "v8",
+      exclude: [
+        "src/main.tsx", //entry point
+        "src/app/App.tsx", //just the app wrapper
+        "src/config/**", //configuration
+        "src/features/**/types/**", //type definitions
+        "src/api/**",
+      ],
       reporter: ["text", "text-summary", "lcov", "html", "cobertura"],
       reportsDirectory: "./coverage",
     },
