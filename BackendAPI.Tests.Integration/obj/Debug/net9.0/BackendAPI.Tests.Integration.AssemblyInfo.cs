92ef551b79aafd3fe6aa043e616dcb8cdda22206
Cecilie J <cecilie@gmail.com>
2025-11-13 13:33:55 +0100

    Added new folder for diagrams and sd for backend login

--- BackendAPI.Tests.Integration/obj/Debug/net9.0/BackendAPI.Tests.Integration.AssemblyInfo.cs
+++ BackendAPI.Tests.Integration/obj/Debug/net9.0/BackendAPI.Tests.Integration.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackendAPI.Tests.Integration")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9c8882b96ac15d5af6204164bb8b6a5abeaf163")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd728012fd5a471f88fbd58ee2f011ab6a0bd080")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackendAPI.Tests.Integration")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackendAPI.Tests.Integration")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
